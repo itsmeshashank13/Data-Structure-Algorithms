0b3581e528f6e10148be7d09a5663063f336686f
Shashank Tiwari <shashanktiwari1312@gmail.com>
2024-01-19 21:44:33 +0530

    warmUp1 problem3

---
+++ CodingBat/WarmUp1/sumDouble.java
@@ -0,0 +1,18 @@
+/* Q. 
+    Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+
+
+    sumDouble(1, 2) → 3
+    sumDouble(3, 2) → 5
+    sumDouble(2, 2) → 8
+ */
+
+ public int sumDouble(int a, int b) {
+    int sum = a+b;
+    
+    if (a == b) {
+      return sum = sum*2;
+    } else {
+      return sum;
+    }
+}
