7a96015ec6fc6fa3a213ca150709136f8bf1d6d5
Shashank Tiwari <shashanktiwari1312@gmail.com>
2024-01-22 22:19:54 +0530

    problem 10

---
+++ CodingBat/WarmUp1/missingChar.java
@@ -0,0 +1,26 @@
+/* Q. 
+    Given a non-empty string and an int n, return a new string where the char at index n has been removed. The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..str.length()-1 inclusive).
+
+
+    missingChar("kitten", 1) → "ktten"
+    missingChar("kitten", 0) → "itten"
+    missingChar("kitten", 4) → "kittn"
+ */
+
+
+public class missingChar {
+
+    public String missingChar(String str, int n) {
+        if (n >= 0 && n < str.length()) {
+          return str.substring(0, n) + str.substring(n + 1);
+        }
+          return str;
+    }
+
+
+    public static void main(String[] args) {
+        missingChar obj = new missingChar();
+        String result = obj.missingChar("Hello", 1);
+        System.out.println(result);
+    }
+}
