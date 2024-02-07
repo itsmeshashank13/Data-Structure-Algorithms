55cf3489e3e8dc1289f23ce7df44c258cb53ea6a
Shashank Tiwari <shashanktiwari1312@gmail.com>
2024-02-07 22:52:56 +0530

    problem 21

---
+++ CodingBat/WarmUp1/delDel.java
@@ -0,0 +1,25 @@
+/* Q.
+    Given a string, if the string "del" appears starting at index 1, return a string where that "del" has been deleted. Otherwise, return the string unchanged.
+
+
+    delDel("adelbc") → "abc"
+    delDel("adelHello") → "aHello"
+    delDel("adedbc") → "adedbc"
+ */
+
+public class delDel {
+    public String delDel(String str) {
+        if (str.length() >= 4 && str.substring(1, 4).equals("del")) {
+           return str.substring(0,1) + str.substring(4);   
+        } else {
+          return str;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        delDel obj = new delDel();
+        String result = obj.delDel("adelbc");
+        System.out.println(result);
+    }
+}
